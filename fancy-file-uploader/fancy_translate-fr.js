71127a5d1e65e15e9544291010be136edc24d765
Ttipi <55588911+Ttipi@users.noreply.github.com>
2021-02-19 12:42:27 +0100

    Add files via upload

---
+++ fancy-file-uploader/fancy_translate-fr.js
@@ -0,0 +1,53 @@
+$.fn.FancyFileUpload.defaults.langmap = {
+	'There is a file upload still in progress.  Leaving the page will cancel the upload.\n\nAre you sure you want to leave this page?': 
+                "Il y a toujours un fichier en cours de transmission. Quitter la page risque d'annuler le transfert.\n\nEtes-vous sûr de vouloir partir ?",
+        'There is a file that was added to the queue but the upload has not been started.  Leaving the page will clear the queue and not upload the file.\n\nAre you sure you want to leave this page?':
+                "Un fichier a été ajouté à la file d'attente mais le transfert n'a pas démarré. Quitter la page risque de supprimer cette file d'attente.\n\nEtes-vous sûr de vouloir partir ?",
+        'Starting upload...': 
+                "Démarrage du transfert...",
+        'Cancel upload and remove from list': 
+                "Annuler le transfert et supprimer de la liste",
+        'This file is currently being uploaded.\n\nStop the upload and remove the file from the list?':
+                "Ce fichier a déjà été transféré.\n\nStopper son transfert et supprimer ce fichier de la liste ?",
+        'This file is waiting to start.\n\nCancel the operation and remove the file from the list?':
+                "Ce fichier attend de démarrer.\n\nAnnuler l'opération et supprimer le fichier de la liste ?",
+        'Preview':
+                "Visualisation",
+        'No preview available':
+                "Pas de visualisation disponible",
+        'File is too large.  Maximum file size is {0}.':
+                "Le fichier est trop volumineux. La taille maximale est {0}.",
+        'Start uploading':
+                "Démarrage du transfert",
+        'Remove from list':
+                "Supprimer de la liste",
+        '{0} of {1} | {2}%':
+                "{0} sur {1} | {2}%",
+        '{0} | Network error, retrying in a moment... ({1})':
+                "{0} | erreur réseau, retenter dans un moment... ({1})",
+        'The upload was cancelled.':
+                "Le transfert a été annulé.",
+        'The upload failed.  {0} ({1})':
+                "Le transfert a échoué.  {0} ({1})",
+        'The upload failed.':
+                "Le transfert a échoué.",
+        'Remove from list':
+                "Enlever de la liste",
+        'The server indicated that the upload was not successful.  No additional information available.':
+                "Le serveur indique que la transfert a échoué. Pas plus d'information disponible.",
+        'Upload completed':
+                "Transfert réussi.",
+        'Browse, drag-and-drop, or paste files to upload':
+                "Sélectionner, glisser ou coller des fichiers pour les transférer",
+        'Audio recording - {0}.mp3':
+                "Enregistrement audio - {0}.mp3",
+        'Unable to record audio.  Either a microphone was not found or access was denied.':
+                "Enregistrement audio impossible. Soit le micro n'a pas été trouvé, soit son accès n'est pas permis.",
+        'Record video using a camera':
+                "Enregistrer la vidéo avec votre caméra",
+        'Video recording - {0}.mp4':
+                "Vidéo en cours d'enregistrement - {0}.mp4",
+        'Unable to record video.  Either a camera was not found or access was denied.':
+                "Impossible d'enregistrer la vidéo. Soit la caméra n'a pas été trouvée, soit son accès n'est pas permis.",
+        
+};
